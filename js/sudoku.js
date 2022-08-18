3a1cbe2e0566cf50742b7396e7f3e59edc19a7b8
elpguillen <elpguillen@users.noreply.github.com>
2022-08-18 05:32:13 -0700

    Add implementation to play an Easy Sudoku puzzle. Add input color change for inputs that violate Sudoku constraints.

--- js/sudoku.js
+++ js/sudoku.js
@@ -62,29 +62,22 @@ const sudoku_puzzle_error = [
     `
 ];
 
-let has_cell_value = [
-    0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0
-];
-
 let num_filled_cells = 0;
+let numBadInputs = 0;
+const numberPuzzleRows = 9;
+const numberPuzzleCols = 9;
+const total_number_cells =  numberPuzzleRows * numberPuzzleCols;
+let cellsWithBadInput = Array(total_number_cells).fill(false);
+let has_cell_value = Array(total_number_cells).fill(0);
 const sudokuContainer = document.getElementsByClassName("sudoku-table")[0];
 // Start making grid for the Sudoku
 makeSudokuTable(sudokuContainer);
 
 addPuzzleValues();
-//console.log(passRowConstraint(9, 7, 4));
-//console.log(passGridConstraint(9, 9, 6, 5, 5));
-console.log(passSudokuConstraints(9, 9, 7, 7, 7));
+addCellInputEventListeners();
 
-
+/*-------------------------------- functions --------------------------------*/
+   
 /**
  * Creates a 9x9 Sudoku grid
  * @param {element} element HTML element that will hold the Sudoku grid
@@ -144,6 +137,7 @@ function addPuzzleValues() {
         if (cell_input_value != 0) {
             cell_input.value = cell_input_value;
             cell_input.setAttribute("readonly", true);
+            cell_input.style.fontWeight = 'bold';
             ++num_filled_cells;
             has_cell_value[col] = 1;
         } else {
@@ -155,19 +149,21 @@ function addPuzzleValues() {
 /**
  * Checks that the value inserted in the row is unique.
  * @param   {Number} numberCols     Total number of columns
- * @param   {Number} currentRow     The row that value was inserted
+ * @param   {Number} currentRow     The row value was inserted
+ * @param   {Number} currentCol     The column value was inserted
  * @param   {Number} numberInserted Value that was inserted
  * @returns {Boolean}               True if value unique, False otherwise
  */
-function passRowConstraint(numberCols, currentRow, numberInserted) {
+function passRowConstraint(numberCols, currentRow, currentCol, numberInserted) {
 
     if ( (typeCheck(numberCols) !== 'number') ||
             (typeCheck(currentRow) !== 'number') ||
+            (typeCheck(currentCol) !== 'number') ||
             (typeCheck(numberInserted) !== 'number')) {
                 return false;
     }
 
-    if (numberCols < 1 || currentRow < 1 ||
+    if (numberCols < 1 || currentRow < 1 || currentCol < 1 ||
         numberInserted < 1 || numberInserted > 9) {
             return false;
     }
@@ -176,6 +172,10 @@ function passRowConstraint(numberCols, currentRow, numberInserted) {
     let lastIndex = firstCellIndex + numberCols;
 
     for (cellIndex = firstCellIndex; cellIndex < lastIndex; cellIndex++) {
+        if ( ((cellIndex % 9) + 1) === currentCol) {
+            continue;
+        }
+
         let cell = document.getElementById("cell[" + cellIndex + "]").firstChild;
         if (parseInt(cell.value, 10) === numberInserted) {
             return false;
@@ -188,30 +188,45 @@ function passRowConstraint(numberCols, currentRow, numberInserted) {
 /**
  * Checks that the value inserted in the column is unique.
  * @param   {Number} numberRows     Total number of rows
- * @param   {Number} currentCol     The column that value was inserted
+ * @param   {Number} currentRow     The row value was inserted
+ * @param   {Number} currentCol     The column value was inserted
  * @param   {Number} numberInserted Value that was inserted
  * @returns {Boolean}               True if value unique, False otherwise
  */
-function passColConstraint(numberRows, currentCol, numberInserted) {
+function passColConstraint(numberRows, currentRow, currentCol, numberInserted) {
     
     if ( (typeCheck(numberRows) !== 'number') ||
+            (typeCheck(currentRow) !== 'number') ||
             (typeCheck(currentCol) !== 'number') ||
             (typeCheck(numberInserted) !== 'number')) {
                 return false;
     }
 
-    if (numberRows < 1 || currentCol < 1 ||
+    if (numberRows < 1 || currentCol < 1 || currentRow < 1 ||
         numberInserted < 1 || numberInserted > 9) {
             return false;
     }
 
     let firstCellIndex = (currentCol - 1);
     let lastIndex = (numberRows * 8) + firstCellIndex + 1;
+    //console.log("firstCell: " + firstCellIndex);
+    //console.log("lastIndex: " + lastIndex);
 
     for (cellIndex = firstCellIndex; cellIndex < lastIndex;) {
-        let cell = document.getElementById("cell[" + cellIndex + "]").firstChild;
-        if (parseInt(cell.value, 10) === numberInserted) {
-            return false;
+        let ind = Math.floor(cellIndex / 9) + 1;
+        //console.log("passCol: " + ind);
+        //console.log("cellIndex: "  + cellIndex);
+
+        if ( (Math.floor(cellIndex / 9) + 1) === currentRow) {
+            //console.log(currentRow);
+            // continue was causing an infinite loop, might want to take
+            // a look at this
+            //continue;
+        } else {
+            let cell = document.getElementById("cell[" + cellIndex + "]").firstChild;
+            if (parseInt(cell.value, 10) === numberInserted) {
+                return false;
+            }
         }
         cellIndex = cellIndex + 9;
     }
@@ -250,6 +265,10 @@ function passGridConstraint(numberRows, numberCols, currentRow, currentCol, numb
 
     for (row = rowStart; row < rowEnd; row++) {
         for (col = colStart; col < colEnd; col++) {
+
+            if ( (row === currentRow) && (col === currentCol))
+                continue;
+
             let cellIndex = (row * 9) + col;
             let cell = document.getElementById("cell[" + cellIndex + "]").firstChild;
 
@@ -286,10 +305,94 @@ function passSudokuConstraints(numberRows, numberCols, rowIndex, colIndex, cellV
             return false;
     }
 
-    return passRowConstraint(numberCols, rowIndex, cellValue) &&
-            passColConstraint(numberRows, colIndex, cellValue) &&
+    return passRowConstraint(numberCols, rowIndex, colIndex, cellValue) &&
+            passColConstraint(numberRows, rowIndex, colIndex, cellValue) &&
             passGridConstraint(numberRows, numberCols, rowIndex, colIndex, cellValue);
 }
+
+function addCellInputEventListeners() {
+    // get all cell input elements
+    const inputTags = document.querySelectorAll('input');
+    const numInputTags = inputTags.length;
+
+    // add event listener to each cell input element
+    for (let i = 0; i < numInputTags; i++) {
+        inputTags[i].addEventListener('input', e => {
+
+            let cell = document.getElementById("cell[" + i + "]").firstChild;
+            let cellValue = parseInt(cell.value);
+            //console.log("value inserted: " + cellValue);
+            let row = Math.floor(i / 9) + 1;
+            let column = (i % 9) + 1;
+            //console.log(passSudokuConstraints(9, 9, row, column, cellValue));
+
+            let status = document.getElementsByClassName('sudoku-status')[0];
+
+            if (has_cell_value[i] === 0) {
+                has_cell_value[i] = 1;
+                ++num_filled_cells;
+                //console.log("Value added");
+            } else {
+                has_cell_value[i] = 0;
+                --num_filled_cells;
+                //console.log("Value deleted")
+            }
+
+            if (!passSudokuConstraints(9, 9, row, column, cellValue)) {
+                updateInputCell(i, false);
+                console.log("cells filled: " + num_filled_cells);
+                status.textContent = `Status: Error at row: ${row}, column:${column}`;
+            } else {
+                updateInputCell(i, true);
+                console.log("cells filled: " + num_filled_cells);
+
+                if (numBadInputs === 0) {
+                    status.textContent = "Status: Okay";
+                }
+            }
+
+            //console.log("position:[" + row + "]" + "[" + column + "]");
+
+            
+            //console.log("-------");
+
+            if (num_filled_cells === 81 && numBadInputs === 0) {
+                status.textContent = "Solved!";
+            }
+        });
+    }
+}
+
+function updateInputCell(cellPosition, statusGood) {
+    if ( (typeCheck(cellPosition) !== 'number') || (typeCheck(statusGood) !== 'boolean') ) {
+        return;
+    }
+
+    let cell = document.getElementById("cell[" + cellPosition + "]").firstChild;
+
+    if (statusGood) {
+        if (cellsWithBadInput[cellPosition] === true) {
+            cellsWithBadInput[cellPosition] = false;
+            --numBadInputs;
+            cell.style.backgroundColor = 'white';
+        }
+    } else {
+        cellsWithBadInput[cellPosition] = true;
+        ++numBadInputs;
+        cell.style.backgroundColor = '#ff2400';
+    }
+    console.log(numBadInputs);
+}
+
+/*function rowColToIndex(rowIndex, colIndex) {
+
+    if ( (typeCheck(rowIndex) !== 'number') || (typeCheck(colIndex) !== 'number') ||
+            rowIndex < 1 || colIndex < 1 || rowIndex > 9 || colIndex > 9) {
+        return -1;
+    }
+
+    return ((rowIndex - 1) * 9) + (colIndex - 1);
+}*/
 
 // https://www.freecodecamp.org/news/javascript-typeof-how-to-check-the-type-of-a-variable-or-object-in-js/
 function typeCheck(value) {
